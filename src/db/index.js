4f97d8e6559eaf29d6c7c7c9105fcc3b2a418782
md <hasnainshaikh62479@gmail.com>
2024-03-01 02:00:59 +0530

    done

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`mongodb+srv://hasnainshaikh62479:hasnainshaikh62479@cluster0.2su9nuf.mongodb.net/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ytproject`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
         process.exit(1)
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config({
 })
 connectDB().then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
-        console.log(` Server is running at port : 8000`);
+        console.log(` Server is running at port : ${process.env.PORT}`);
     })
 }).catch((error)=>{
     console.log("MONGODB connection failed !!! ", error);
